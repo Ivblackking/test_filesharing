28e90145fb619c96b653c4de219abdcb8d49cdb6
Ivblackking <ivan.lysenko305@gmail.com>
2024-11-13 22:20:07 +0200

    added models

---
+++ backend/models.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column, Boolean, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True)
+    password = Column(String(256))
+    is_admin = Column(Boolean, default=False)
+    files = relationship('MyFile', secondary='users_files', back_populates='users')
+
+
+class MyFile(Base):
+    __tablename__ = "files"
+
+    id = Column(Integer, primary_key=True, index=True)
+    filepath = Column(String(256))
+    users = relationship('User', secondary='users_files', back_populates='files')
+
+
+class UsersFiles(Base):
+    __tablename__ = "users_files"
+
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
+    file_id = Column(Integer, ForeignKey("files.id", ondelete="CASCADE"), primary_key=True)
