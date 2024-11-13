9276783b8f945128a757169d4a7a4b9d99a0d921
Ivblackking <ivan.lysenko305@gmail.com>
2024-11-13 20:02:43 +0200

    updated database file

--- backend/database.py
+++ backend/database.py
@@ -12,10 +12,9 @@ db_port = os.environ.get("WEB_MYSQL_TCP_PORT")
 db_name = os.environ.get("WEB_MYSQL_DB")
 
 DATABASE_URL = f"mysql+pymysql://{db_username}:{db_password}@filesharing_db:{db_port}/{db_name}"
-print(DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
 
-Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
