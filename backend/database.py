49d95976981ba4fd41ecb5617ea089e60b03ba46
Ivblackking <ivan.lysenko305@gmail.com>
2024-11-14 11:09:51 +0200

    updated docker-compose, requirements, database

--- backend/database.py
+++ backend/database.py
@@ -1,17 +1,17 @@
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-load_dotenv()
+load_dotenv(find_dotenv())
 
 db_username = os.environ.get("WEB_MYSQL_USER")
 db_password = os.environ.get("WEB_MYSQL_PASSWORD")
 db_port = os.environ.get("WEB_MYSQL_TCP_PORT")
 db_name = os.environ.get("WEB_MYSQL_DB")
 
-DATABASE_URL = f"mysql+pymysql://{db_username}:{db_password}@filesharing_db:{db_port}/{db_name}"
+DATABASE_URL = f'mysql+pymysql://{db_username}:{db_password}@webdb:{db_port}/{db_name}'
 
 engine = create_engine(DATABASE_URL)
 
